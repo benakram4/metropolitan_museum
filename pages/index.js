36e33f99c58ed104d173adf4a1a80dee88bc232b
benakram4 <bakram4@myseneca.ca>
2023-04-05 18:33:45 -0400

    alt text for cover photo

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
       <main>
         <Row>
           <Col className='mb-4'>
-            <Image alt='' src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" fluid rounded />
+            <Image alt='Metropolitan Museum of Art' src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" fluid rounded />
           </Col>
         </Row>
         <Row>
